185e235e20894bc4f784b2211f169fa8d84aa413
Léo Guillaume <leo.guillaume@gmail.com>
2023-05-06 13:13:58 +0200

    feat: star rating component

---
+++ app/src/components/fields/Rating/StarRatings.vue
@@ -0,0 +1,15 @@
+<template>
+  <form class="rating">
+    <div class="flex">
+      <StarRating v-for="nbStar in nbStars" :key="nbStar" :index="nbStar" v-model:current-star="currentStar"/>
+    </div>
+  </form>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+import StarRating from '@/components/fields/Rating/StarRating.vue'
+
+const nbStars = ref(5)
+const currentStar = ref(0)
+</script>
--- app/src/pages/HomePage.vue
+++ app/src/pages/HomePage.vue
@@ -2,14 +2,17 @@
   <section id="home" class="pt-5">
     <h1 v-if="authStore.user">hello {{authStore.user.name}}</h1>
     <RouterLink to="/searches">Recherches</RouterLink>
+    <Ratings  v-model="ratingValue"/>
   </section>
 </template>
 
 <script lang="ts" setup>
 import { SITE_NAME } from "@/env";
 import { useAuthStore } from '@/stores/auth.store'
+import Ratings from '@/components/fields/Rating/StarRatings.vue'
+import { ref } from 'vue'
 /*REFS*/
-
+const ratingValue = ref(0)
 /*STORE*/
 const authStore = useAuthStore()
 
